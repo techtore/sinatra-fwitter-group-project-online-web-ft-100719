41bde45990570c8bf0ad482836dc7a62f5532979
Victoria Hale <vic.hale74@gmail.com>
2019-12-02 19:07:08 +0000

    Automatically backed up by Learn

--- app/models/slugify.rb
+++ app/models/slugify.rb
@@ -6,7 +6,6 @@ module Slugifiable
   end
   module ClassMethods
     def find_by_slug(slug)
-      # is there a better way to do this?
       self.all.detect {|i| i.username.parameterize == slug}
     end
   end
